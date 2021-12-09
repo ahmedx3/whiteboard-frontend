3a8489586e9d1794f33dc83528fe556de2a3b6a9
ahmedmostafa-incorta <ahmed.moustafaa99@gmail.com>
2021-12-09 20:30:13 +0200

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+
+  components: {
+    HelloWorld,
+  },
+};
+</script>
