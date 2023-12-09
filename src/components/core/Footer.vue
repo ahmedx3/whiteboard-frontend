56ac095484a7453533fbbcac26fc320b0796f0ca
Christopher Obiozor <chris@chrisobiozor.com>
2023-12-09 10:28:33 -0500

    Add hard coded version 0.2.0 stamp to Footer.vue

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer id="dashboard-core-footer" height="80">
+  <v-footer id="dashboard-core-footer">
     <v-container>
       <v-row align="center" no-gutters>
         <v-col v-for="(link, i) in links" :key="i" class="text-center mb-sm-0 mb-5" cols="auto">
@@ -11,14 +11,24 @@
           />
         </v-col>
 
+        <v-responsive width="100%"></v-responsive>
+
+        <v-col cols="12" md="auto">
+          <div class="text-body-1 font-weight-light pt-6 pt-md-0 text-center">
+            &copy; {{ new Date().getFullYear() }} Latitude Labs 
+          </div>
+        </v-col>
+
         <v-spacer class="hidden-sm-and-down" />
 
         <v-col cols="12" md="auto">
           <div class="text-body-1 font-weight-light pt-6 pt-md-0 text-center">
-            Made for a College Project &copy; 2022
+            version 0.2.0
           </div>
         </v-col>
+
       </v-row>
+
     </v-container>
   </v-footer>
 </template>
