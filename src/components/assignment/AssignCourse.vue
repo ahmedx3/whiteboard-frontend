36b77b62fc685947b0c10a01b49ff92a3c2ed6ba
Christopher Obiozor <chris@chrisobiozor.com>
2023-12-08 21:48:57 -0500

    Update AssignCourse.vue button template to use vuetify columns

--- src/components/assignment/AssignCourse.vue
+++ src/components/assignment/AssignCourse.vue
@@ -2,126 +2,129 @@
   <div>
     <v-container>
       <!-- Assign Course Dialog Modal -->
+      <!-- @TODO fix justify="end" attribute not working -->
       <v-row
         align="center"
-        justify="center"
+        justify="end"
         v-if="
           ($store.state.currentUser && $store.state.currentUser.type === 'instructor') ||
           ($store.state.currentUser && $store.state.currentUser.type === 'admin')
         "
       >
-        <v-dialog v-model="dialog" max-width="600px">
-          <!-- assign course button -->
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" v-on="on" @click="setAssignmentOpenDate()" color="#b8860b">
-              <v-icon left> mdi-plus </v-icon>
-              Assign Course
-            </v-btn>
-          </template>
-          <!-- assign course dialog details -->
-          <v-card>
-            <v-card-title>
-              <span class="text-h2 font-weight-normal">New Assignment</span>
-            </v-card-title>
-            <v-card-text>
-              <!-- assign course form fields -->
-              <v-container>
-                <v-row>
-                  <!-- assignment name -->
-                  <v-col cols="12">
-                    <v-form ref="form1">
-                      <v-text-field
-                        label="Assignment Name*"
-                        placeholder="Assignment Name*"
-                        outlined
-                        :rules="rules.name"
-                        dense
-                        v-model="newAssignment.name"
-                      ></v-text-field>
+        <v-col>
+          <v-dialog v-model="dialog" max-width="600px">
+            <!-- assign course button -->
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn v-bind="attrs" v-on="on" @click="setAssignmentOpenDate()" color="#b8860b">
+                <v-icon left> mdi-plus </v-icon>
+                Assign Course
+              </v-btn>
+            </template>
+            <!-- assign course dialog details -->
+            <v-card>
+              <v-card-title>
+                <span class="text-h2 font-weight-normal">New Assignment</span>
+              </v-card-title>
+              <v-card-text>
+                <!-- assign course form fields -->
+                <v-container>
+                  <v-row>
+                    <!-- assignment name -->
+                    <v-col cols="12">
+                      <v-form ref="form1">
+                        <v-text-field
+                          label="Assignment Name*"
+                          placeholder="Assignment Name*"
+                          outlined
+                          :rules="rules.name"
+                          dense
+                          v-model="newAssignment.name"
+                        ></v-text-field>
 
-                    </v-form>
-                  </v-col>
-                  <!-- assignment course -->
-                  <v-col cols="12">
-                    <v-form ref="form2">
-                      <v-select
-                        label="Class*"
-                        :items="classes"
-                        outlined
-                        :rules="rules.name"
-                        dense
-                        v-model="newAssignment.classID"
-                      ></v-select>
-                    </v-form>
-                  </v-col>
-                  <!-- assignment open date -->                  
-                  <v-col cols="12">
-                    <v-form ref="form3">
-                      <!-- set text field type to datetime-local to get datetime picker
-                      and validation -->
-                      <v-text-field
-                        type="datetime-local"
-                        label="Open Date*"
-                        outlined
-                        dense
-                        v-model="newAssignment.openDate"
-                      ></v-text-field>
-                    </v-form>
-                  </v-col>
-                  <!-- assignment due date -->                  
-                  <v-col cols="12">
-                    <v-form ref="form4">
-                      <!-- set text field type to datetime-local to get datetime picker
-                      and validation -->
-                      <v-text-field
-                        type="datetime-local"
-                        label="Due Date"
-                        outlined
-                        dense
-                        v-model="newAssignment.dueDate"
-                      ></v-text-field>
-                    </v-form>
-                  </v-col>
-                  
-                  <!-- course description -->
-                  <!-- <v-col cols="12">
-                    <v-form ref="form2">
-                      <v-text-field
-                        label="Course Description*"
-                        placeholder="Course Description*"
-                        outlined
-                        :rules="rules.name"
-                        dense
-                        v-model="newAssignment.description"
-                      ></v-text-field>
-                    </v-form>
-                  </v-col> -->
-                  
-                  <!-- course difficulty -->
-                  <!-- <v-col cols="12">
-                    <v-form ref="form3">
-                      <v-select
-                        :items="['Beginner', 'Intermediate', 'Advanced']"
-                        label="Difficulty*"
-                        outlined
-                        dense
-                        :rules="rules.name"
-                        v-model="newAssignment.difficulty"
-                      ></v-select>
-                    </v-form>
-                  </v-col> -->
-                </v-row>
-              </v-container>
-              <small>*indicates required field</small>
-            </v-card-text>
-            <!-- assign course form submit actions -->
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="red" outlined @click="dialog = false"> Close </v-btn>
-              <v-btn color="#b8860b" @click="createAssignment()"> Assign </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+                      </v-form>
+                    </v-col>
+                    <!-- assignment course -->
+                    <v-col cols="12">
+                      <v-form ref="form2">
+                        <v-select
+                          label="Class*"
+                          :items="classes"
+                          outlined
+                          :rules="rules.name"
+                          dense
+                          v-model="newAssignment.classID"
+                        ></v-select>
+                      </v-form>
+                    </v-col>
+                    <!-- assignment open date -->                  
+                    <v-col cols="12">
+                      <v-form ref="form3">
+                        <!-- set text field type to datetime-local to get datetime picker
+                        and validation -->
+                        <v-text-field
+                          type="datetime-local"
+                          label="Open Date*"
+                          outlined
+                          dense
+                          v-model="newAssignment.openDate"
+                        ></v-text-field>
+                      </v-form>
+                    </v-col>
+                    <!-- assignment due date -->                  
+                    <v-col cols="12">
+                      <v-form ref="form4">
+                        <!-- set text field type to datetime-local to get datetime picker
+                        and validation -->
+                        <v-text-field
+                          type="datetime-local"
+                          label="Due Date"
+                          outlined
+                          dense
+                          v-model="newAssignment.dueDate"
+                        ></v-text-field>
+                      </v-form>
+                    </v-col>
+                    
+                    <!-- course description -->
+                    <!-- <v-col cols="12">
+                      <v-form ref="form2">
+                        <v-text-field
+                          label="Course Description*"
+                          placeholder="Course Description*"
+                          outlined
+                          :rules="rules.name"
+                          dense
+                          v-model="newAssignment.description"
+                        ></v-text-field>
+                      </v-form>
+                    </v-col> -->
+                    
+                    <!-- course difficulty -->
+                    <!-- <v-col cols="12">
+                      <v-form ref="form3">
+                        <v-select
+                          :items="['Beginner', 'Intermediate', 'Advanced']"
+                          label="Difficulty*"
+                          outlined
+                          dense
+                          :rules="rules.name"
+                          v-model="newAssignment.difficulty"
+                        ></v-select>
+                      </v-form>
+                    </v-col> -->
+                  </v-row>
+                </v-container>
+                <small>*indicates required field</small>
+              </v-card-text>
+              <!-- assign course form submit actions -->
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="red" outlined @click="dialog = false"> Close </v-btn>
+                <v-btn color="#b8860b" @click="createAssignment()"> Assign </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-col>
       </v-row>
     </v-container>
   </div>
